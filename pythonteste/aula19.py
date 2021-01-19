ce13ce0bee4341cafd318597cb469e2caccde1a7
Thiago Ferreira <thiaago.dev@gmail.com>
2021-01-19 18:45:57 -0300

    Aula 19

---
+++ pythonteste/aula19.py
@@ -0,0 +1,25 @@
+pessoas = {
+    'nome': 'Thiago',
+    'sexo': 'M',
+    'idade': 17
+}
+
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+
+del pessoas['sexo']
+pessoas['idade'] = 18
+pessoas['peso'] = 50
+
+for key, value in pessoas.items():
+    print(f'{key} = {value}')
+
+brasil = []
+estado = {}
+for i in range(0, 3):
+    estado['uf'] = input('Unidade Federativa: ')
+    estado['sigla'] = input('Sigla: ')
+    brasil.append(estado.copy())
+
+print(brasil)
